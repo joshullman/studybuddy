7ff9cf91fe49fa3e15e2f4c92b76177115e00133
Arielle Chen & Vivek Poola <pair+achen116.vpoola88@devbootcamp.com>
2015-08-15 15:56:39 -0700

    add teacher and student sign up routes and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :teachers, except: [:new]
+  get 'teachers/signup' => 'teachers#new'
+  get 'students/signup' => 'students#new'
 
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  delete 'signout' => 'sessions#destroy'
 end
