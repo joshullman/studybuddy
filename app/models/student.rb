1414106e182ccb6c1127aa875a3155f48878a99c
Arielle Chen & Vivek Poola <pair+achen116.vpoola88@devbootcamp.com>
2015-08-15 17:51:10 -0700

    fix merge conflict with student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,10 +7,10 @@ class Student < ActiveRecord::Base
 	has_many :student_assignments
 	has_many :assignments, through: :student_assignments
 
-<<<<<<< HEAD
+
   has_secure_password
-=======
+
 	has_many :sent_feedbacks, class_name: "Feedback", source: :sender, foreign_key: "sender_id"
   has_many :received_feedbacks, class_name: "Feedback", source: :receiver, foreign_key: "receiver_id"
->>>>>>> 9bbdac202f285263639f8e6ff2f9b64313da3c07
+
 end
