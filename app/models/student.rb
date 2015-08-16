28e9e668eed685d0f08ed67d8fb843d66a9c56a5
Josh Ullman <joshullman@gmail.com>
2015-08-15 17:10:11 -0700

    Add functioning seed

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,6 +7,6 @@ class Student < ActiveRecord::Base
 	has_many :student_assignments
 	has_many :assignments, through: :student_assignments
 
-	has_many :sent_feedbacks, class_name: "Feedback", source: :user_one, foreign_key: "sender"
-  has_many :received_feedbacks, class_name: "Feedback", source: :user_two, foreign_key: "receiver"
+	has_many :sent_feedbacks, class_name: "Feedback", source: :sender, foreign_key: "sender_id"
+  has_many :received_feedbacks, class_name: "Feedback", source: :receiver, foreign_key: "receiver_id"
 end
