62a33ed3df28a1ecca5c7596bdc0a1ff17f27533
Arielle Chen & Vivek Poola <pair+achen116.vpoola88@devbootcamp.com>
2015-08-15 22:05:45 -0700

    add create classroom button and start adding students to classroom

---
+++ app/controllers/classroom_students_controller.rb
@@ -0,0 +1,16 @@
+class ClassroomStudentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    # find current classroom
+    # add students into current classroom
+  end
+
+  private
+    def classroom_student_params
+    end
+end
---
+++ app/controllers/classrooms_controller.rb
@@ -0,0 +1,49 @@
+class ClassroomsController < ApplicationController
+  before_action :find_classroom_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def show
+    @classroom_student = ClassroomStudent.new
+    @students = Student.all
+  end
+
+  def new
+    @classroom = Classroom.new
+    @teacher = current_teacher
+  end
+
+  def create
+    @teacher = Teacher.find(params[:teacher_id])
+    @classroom = @teacher.classrooms.new(classroom_params)
+
+    if @classroom.save
+      render 'show'
+    else
+      @error = @classroom.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_classroom_params
+    @classroom = Classroom.find(params[:id])
+  end
+
+  def classroom_params
+    params.require(:classroom).permit(:name, :teacher_id)
+  end
+
+end
